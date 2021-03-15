fde8492462e2f7e8fb19925add1b62c09b436312
benjamindz <dzaferovicbenjamin@gmail.com>
2021-03-15 23:25:49 +0100

    Added function that changes global variable but still returns value undefined

---
+++ ex07/src/index.js
@@ -0,0 +1,18 @@
+var sum = 0;
+
+function addThree() {
+    sum = sum + 3;
+}
+
+function addFive() {
+    sum = sum + 5;
+}
+
+console.log(addThree());
+console.log(addFive());
+
+
+module.exports = {
+    addThree,
+    addFive
+};
