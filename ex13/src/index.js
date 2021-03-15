aa7199bc226a44a10348e7ed6ab222bd48ad94a6
benjamindz <dzaferovicbenjamin@gmail.com>
2021-03-16 00:54:31 +0100

    Created function that generates random number in range

---
+++ ex13/src/index.js
@@ -0,0 +1,6 @@
+function randomRangeNumber(minNumber, maxNumber) {
+    return Math.floor(Math.random() * (maxNumber - minNumber +1)) + minNumber;
+}
+
+console.log(randomRangeNumber(1, 50));
+module.exports = randomRangeNumber;
